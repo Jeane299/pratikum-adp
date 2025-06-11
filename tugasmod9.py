7c9afe90135a39737b605a5c9cbb6dcec63a636d
Jeane299 <dwisarijeane@gmail.com>
2025-06-11 21:12:20 +0700

    Add files via upload

--- tugasmod9.py
+++ tugasmod9.py
@@ -1,17 +1,23 @@
 import os 
+import time
 from termcolor import colored, cprint
 os.system('cls')
 for j in range (3):
     cprint("|", 'white', 'on_black', end="")
+    time.sleep(0.2)
     cprint(" "*20, 'white', 'on_red', end="")
+    time.sleep(0.2)
     print()
 for j in range (3):
     cprint("|", 'white', 'on_black', end="")
+    time.sleep(0.2)
     cprint(" "*20, 'white', 'on_white', end="")
+    time.sleep(0.2)
     print()
 
 for j in range (10):
     cprint("|", 'white', 'on_black', end="")
+    time.sleep(0.2)
     print()
 # import os
 # from termcolor import colored, cprint
@@ -23,5 +29,6 @@ for j in range (10):
 #     cprint(" "*10, 'white', 'on_blue', end="")
 #     cprint(" "*10, 'white', 'on_yellow', end="")
 #     print()
+
 
 
